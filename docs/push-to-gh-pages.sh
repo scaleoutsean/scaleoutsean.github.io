1458245cc62f68966c5e1b7015c9b94911fd9a2a
scaleoutSean <scaleoutsean@users.noreply.github.com>
2020-11-14 22:24:51 +0800

    First test fix

--- docs/push-to-gh-pages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git diff-tree -r --name-only --no-commit-id master | grep '^_site/' &> /dev/null
-if [ $? == 0 ]; then
-  git push origin `git subtree split --prefix site master 2> /dev/null`:gh-pages --force
-fi
