c691de3f910e62a8b0b3537000ef7b30bd141347
scaleoutSean <scaleoutsean@users.noreply.github.com>
2020-11-16 18:26:39 +0800

    Update for search

--- docs/prepare.sh
+++ docs/prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
-bundle exec jekyll b
-touch _site/.nojekyll
+bundle exec jekyll b --incremental
 rm _site/jekyll-yamt.gemspec
-rsync -rav _site/ docs/
+touch _site/.nojekyll
+cp LICENSE.txt _site/
+sed -i 's/localhost:4000/scaleoutsean.github.io/g' _site/sitemap.xml
+# Ready go commit and push
+rsync -rav --delete _site/ docs/
