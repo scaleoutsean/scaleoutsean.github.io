2001bb30abca488f1ce69bdc78cd0a8d0aa1069f
scaleoutSean <scaleoutsean@users.noreply.github.com>
2021-03-08 22:56:51 +0800

    Final fix for JS files?

---
+++ docs/trident/_static/algolia.js
@@ -0,0 +1,10 @@
+docsearch({
+apiKey: '4a93c637dab1729719467f0c70a0d4a1',
+indexName: 'trident',
+inputSelector: '#search-input',
+algoliaOptions: { 
+  'hitsPerPage': 10
+  },
+// Set debug to true if you want to inspect the dropdown
+debug: false,
+});
