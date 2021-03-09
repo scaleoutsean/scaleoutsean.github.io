261d6386f0a9d4adfc80c22155a4d62ed3dec080
scaleoutSean <scaleoutsean@users.noreply.github.com>
2021-03-09 13:41:53 +0800

    Search tuning

--- docs/trident/_static/algolia.js
+++ docs/trident/_static/algolia.js
@@ -1,8 +1,26 @@
 docsearch({
 apiKey: '4a93c637dab1729719467f0c70a0d4a1',
 indexName: 'trident',
+// search.html has no ID ("search-input") like side menu
 inputSelector: '#search-input',
+transformData: function(hits) {
+      hits.map(hit => {
+      	if (hit.anchor) {
+        	hit.url = hit.url.replace('/html', '')
+        	hit.anchor = null;
+         }
+      })
+      return hits;
+    },
+if (context.selectionMethod === 'click') {
+   input.setVal('');
+   const windowReference = window.open(suggestion.url, '_blank');
+     windowReference.focus();
+   },
 algoliaOptions: { 
+  'queryLanguages': 'en',
+  'ignorePlurals': true,
+  'removeStopWords': true,
   'hitsPerPage': 10
   },
 // Set debug to true if you want to inspect the dropdown
