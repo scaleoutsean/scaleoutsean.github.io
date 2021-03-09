3eae042362106126b44d1e94af372731b9f76368
scaleoutSean <scaleoutsean@users.noreply.github.com>
2021-03-09 14:30:29 +0800

    Moar JS experiments

--- docs/trident/_static/algolia.js
+++ docs/trident/_static/algolia.js
@@ -1,28 +1,29 @@
 docsearch({
-apiKey: '4a93c637dab1729719467f0c70a0d4a1',
-indexName: 'trident',
-// search.html has no ID ("search-input") like side menu
-inputSelector: '#search-input',
-transformData: function(hits) {
-      hits.map(hit => {
-      	if (hit.anchor) {
-        	hit.url = hit.url.replace('/html', '')
-        	hit.anchor = null;
-         }
-      })
-      return hits;
-    },
-if (context.selectionMethod === 'click') {
-   input.setVal('');
-   const windowReference = window.open(suggestion.url, '_blank');
-     windowReference.focus();
-   },
-algoliaOptions: { 
-  'queryLanguages': 'en',
-  'ignorePlurals': true,
-  'removeStopWords': true,
-  'hitsPerPage': 10
+  apiKey: '4a93c637dab1729719467f0c70a0d4a1',
+  indexName: 'trident',
+  inputSelector: '#search-input',
+  transformData: function(hits) {
+    // modify hits
+    hits.map(hit => {
+      if (hit.anchor) {
+        hit.url = hit.url.replace('/html', '')
+        hit.anchor = null;
+       }
+    })
+    return hits;
+},
+handleSelected: function(input, event, suggestion, datasetNumber, context) {
+  if (context.selectionMethod === 'click') {
+    input.setVal('');
+    const windowReference = window.open(suggestion.url, '_blank');
+    windowReference.focus();
+  }
+},
+  algoliaOptions: {
+    'queryLanguages': 'en',
+    'ignorePlurals': true,
+    'removeStopWords': true,
+    'hitsPerPage': 10
   },
-// Set debug to true if you want to inspect the dropdown
-debug: false,
+  debug: false,
 });
