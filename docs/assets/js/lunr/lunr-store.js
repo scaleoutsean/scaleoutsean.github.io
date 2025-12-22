f4e51af83b59c30a180d2dc6fc085005dfb5e3d2
scaleoutSean <scaleoutsean@users.noreply.github.com>
2025-12-22 16:00:49 +0800

    Kinetica - add flow diagram

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2034,7 +2034,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year projects solidfire random kubernetes virtualization automation storage analytics containers cloud meta openstack linux monitoring backup ai netapp hci netapp hci performance e-series Home Home Home Home Home Home Home Home Home Home...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Kinetica with NetApp E-Series 2025-12-22 00:00 4 minute read Why put Kinetic disk and persist tier on EF storage arrays Resource Provisioning on E-Series SANtricity 2025-12-17 00:00 4 minute read About resource provsioned volumes and groups/pools E-Series SANtricity Web UI lockout 2025-12-05 00:00 2 minute read How to use (or...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Kinetica with NetApp E-Series 2025-12-22 00:00 5 minute read Why put Kinetic disk and persist tier on EF storage arrays Resource Provisioning on E-Series SANtricity 2025-12-17 00:00 4 minute read About resource provsioned volumes and groups/pools E-Series SANtricity Web UI lockout 2025-12-05 00:00 2 minute read How to use (or...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
