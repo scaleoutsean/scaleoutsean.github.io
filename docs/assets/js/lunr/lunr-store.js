6b6827b507d89e6b3643ba814676be7c29abd211
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-02-27 01:53:58 +0800

    Fix various typos, improve NVMe/RoCE post

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2196,7 +2196,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"IBM Block Storage CSI driver patched for NetApp E-Series 2026-02-26 00:00 10 minute read Unofficial patch to IBM Block CSI driver for E-Series SANtricity storage Connecting to 200G HICs on E-Series EF600 2026-02-19 00:00 2 minute read Connecting to EF-Series EF600 200G HIC in DAS mode Linux, NVMe/RoCE and HA...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"IBM Block Storage CSI driver patched for NetApp E-Series 2026-02-26 00:00 11 minute read Unofficial patch to IBM Block CSI driver for E-Series SANtricity storage Connecting to 200G HICs on E-Series EF600 2026-02-19 00:00 2 minute read Connecting to EF-Series EF600 200G HIC in DAS mode Linux, NVMe/RoCE and HA...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
