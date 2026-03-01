efd1219244806ea7c1397dbc173f6077f5053ecd
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-03-01 19:47:34 +0800

    PVE with EF600 - add tuning points

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2208,7 +2208,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Proxmox PVE 9.1.1 (Debian) with NetApp EF-Series NVMe/RoCE 2026-03-01 00:00 8 minute read PVE 9.1.1 with NVMe/RoCE on NetApp EF600 Block external sites to improve loading speed of NetApp docs 2026-03-01 00:00 1 minute read Block external domains on https://docs.netapp.com IBM Block Storage CSI driver patched for NetApp E-Series 2026-02-26...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Proxmox PVE 9.1.1 (Debian) with NetApp EF-Series NVMe/RoCE 2026-03-01 00:00 9 minute read PVE 9.1.1 with NVMe/RoCE on NetApp EF600 Block external sites to improve loading speed of NetApp docs 2026-03-01 00:00 1 minute read Block external domains on https://docs.netapp.com IBM Block Storage CSI driver patched for NetApp E-Series 2026-02-26...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
