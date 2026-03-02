aace64204bf40a45efed56ddd72a7c92743ed297
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-03-02 22:25:06 +0800

    k0rdent/k0s - minor update

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2214,7 +2214,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"k0rdent k0s, SANtricity CSI, NetApp E-Series 2026-03-02 00:00 8 minute read What is k0rdent, what is k0s, and how SANtricity CSI helps you connect them to E-Series storage Proxmox PVE 9.1.1 (Debian) with NetApp EF-Series NVMe/RoCE 2026-03-01 00:00 11 minute read PVE 9.1.1 with NVMe/RoCE on NetApp EF600 Block external...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"k0rdent k0s, SANtricity CSI, NetApp E-Series 2026-03-02 00:00 8 minute read What is k0rdent, what is k0s, and how SANtricity CSI helps you connect them to E-Series storage Proxmox PVE 9.1.1 (Debian) with NetApp EF-Series NVMe/RoCE 2026-03-01 00:00 12 minute read PVE 9.1.1 with NVMe/RoCE on NetApp EF600 Block external...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
