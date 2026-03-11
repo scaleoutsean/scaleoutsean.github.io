5c6a5b9415770b3c1cccb0bb5d5fd547ad085be5
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-03-11 18:37:10 +0800

    NFS/RDMA with E-Series (PVE-related) - add NFS data store screenshots

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2250,7 +2250,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Proxmox PVE with NetApp E-Series-backed NFS/RDMA 2026-03-11 00:00 46 minute read How to deploy Linux-based NFS backed by NetApp E-Series Proxmox Backup Server 4.1 with NetApp E-Series 2026-03-08 00:00 39 minute read Proxmox Backup Server on PVE or bare metal servers and NetApp E-Series Versity Gateway, SANtricity CSI, and NetApp...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Proxmox PVE with NetApp E-Series-backed NFS/RDMA 2026-03-11 00:00 47 minute read How to deploy Linux-based NFS backed by NetApp E-Series Proxmox Backup Server 4.1 with NetApp E-Series 2026-03-08 00:00 39 minute read Proxmox Backup Server on PVE or bare metal servers and NetApp E-Series Versity Gateway, SANtricity CSI, and NetApp...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
