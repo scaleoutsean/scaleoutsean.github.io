195383941ef49652492feaaf5703837b8199b06d
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-03-11 17:45:59 +0800

    NFS/RDMA with E-Series (PVE-related)

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2231,6 +2231,12 @@ var store = [{
         "url": "/2026/03/08/proxmox-backup-server-with-netapp-eseries.html",
         "teaser": null
       },{
+        "title": "Proxmox PVE with NetApp E-Series-backed NFS/RDMA",
+        "excerpt":"Introduction Recently I blogged about: PVE with E-Series arrays, with emphasis on NVMe/RoCE, but I also covered other protocols (iSCSI, mostly) Proxmox Backup Server backed by E-Series block with emphasis on NL-SAS disks The first, PVE-related post, commented on CephFS as a file-based alternative, but I thought to spend some...","categories": ["storage","e-series","virtualization"],
+        "tags": ["e-series","backup","pbs","netapp","nfs","roce","rdma","pve","proxmox"],
+        "url": "/2026/03/11/proxmox-pve-nfs-rdma-netapp-eseries.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2244,7 +2250,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Proxmox Backup Server 4.1 with NetApp E-Series 2026-03-08 00:00 39 minute read Proxmox Backup Server on PVE or bare metal servers and NetApp E-Series Versity Gateway, SANtricity CSI, and NetApp E-Series 2026-03-07 00:00 8 minute read Versity and SANtricity CSI make it possible to deploy S3 service on Kubernetes in...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Proxmox PVE with NetApp E-Series-backed NFS/RDMA 2026-03-11 00:00 45 minute read How to deploy Linux-based NFS backed by NetApp E-Series Proxmox Backup Server 4.1 with NetApp E-Series 2026-03-08 00:00 39 minute read Proxmox Backup Server on PVE or bare metal servers and NetApp E-Series Versity Gateway, SANtricity CSI, and NetApp...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
