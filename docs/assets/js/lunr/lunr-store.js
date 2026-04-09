3778eed9bee04d4ca28f4aa0b1644388efa7f5b0
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-04-09 17:38:19 +0800

    SANtricity workload metadata

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2291,6 +2291,12 @@ var store = [{
         "url": "/2026/04/06/epa-eseries-perf-analyzer-354.html",
         "teaser": null
       },{
+        "title": "Applications and Workloads in NetApp SANtricity Web UI",
+        "excerpt":"Introduction Even in SANtricity 12.0x (as versions are now referred to in TFM), you’ll see this offer to define a (presumably “suitable and optimal”) workload for your new volume. There’s a link that takes us to TFM that tells us everything we need to know about it (for SANtricity 11.9x,...","categories": ["storage","e-series","automation","ai","projects"],
+        "tags": ["netapp","santricity","ef-series","workloads"],
+        "url": "/2026/04/09/netapp-santricity-applications-workloads.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2304,7 +2310,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"What is new in E-Series Perf Analyzer v3.5.4 2026-04-06 00:00 4 minute read What was added, updated, removed and why Proxmox Plugin for NetApp E-Series SANtricity 2026-03-31 00:00 11 minute read Why SANtricity Plug-in for Proxmox 9 exists and why SANmox TUI is better The shocking truth about SANtricity snapshots...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Applications and Workloads in NetApp SANtricity Web UI 2026-04-09 00:00 7 minute read Step-by-step guide to using SANtricity workloads in 2026 What is new in E-Series Perf Analyzer v3.5.4 2026-04-06 00:00 4 minute read What was added, updated, removed and why Proxmox Plugin for NetApp E-Series SANtricity 2026-03-31 00:00 11...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
@@ -3817,6 +3823,9 @@ var store = [{
   },{
     "title": "sanmox",
     "excerpt":"","url": "https://scaleoutsean.github.io/tags/sanmox/"
+  },{
+    "title": "workloads",
+    "excerpt":"","url": "https://scaleoutsean.github.io/tags/workloads/"
   },{
     "title": "projects",
     "excerpt":"","url": "https://scaleoutsean.github.io/categories/projects/"
