54990c0fcc13049e5544c0408614db424a99d400
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-04-22 02:58:16 +0800

    SOK (Splunk 10.2) with IBM Block CSI with SANtricity Patch - improve content

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2328,7 +2328,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Splunk Operator for Kubernetes with NetApp E-Series",
-        "excerpt":"Splunk Operator for Kubernetes (SOK) with NetApp E-Series I haven’t used Splunk since this post in late 2023. That post was more about sizing and performance testing, but even that was too much, since no one has ever asked me anything about it. Still, back then, E-Series didn’t have any...","categories": ["e-series","storage","analytics","kubernetes"],
+        "excerpt":"Splunk Operator for Kubernetes (SOK) with NetApp E-Series I haven’t used Splunk since this post in late 2023. That post was more about sizing and performance testing, but even that was too much since no one has ever asked me anything about it. Still, back then, E-Series didn’t have any...","categories": ["e-series","storage","analytics","kubernetes"],
         "tags": ["netapp","splunk","csi","santricity"],
         "url": "/2026/04/21/splunk-kubernetes-operator-netapp-eseries-santricity.html",
         "teaser": null
@@ -2346,7 +2346,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Splunk Operator for Kubernetes with NetApp E-Series 2026-04-21 00:00 5 minute read Splunk 10.2 installed by SOK on k0s with NetApp SANtricity CSI SANtricity Client (Python) update 2026-04-21 00:00 3 minute read Interfaces, snapshots and more Backup SANtricity CSI volume with Velero and Versity S3 Gateway 2026-04-18 00:00 3 minute...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Splunk Operator for Kubernetes with NetApp E-Series 2026-04-21 00:00 6 minute read Splunk 10.2 installed by SOK on k0s with NetApp SANtricity CSI SANtricity Client (Python) update 2026-04-21 00:00 3 minute read Interfaces, snapshots and more Backup SANtricity CSI volume with Velero and Versity S3 Gateway 2026-04-18 00:00 3 minute...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
