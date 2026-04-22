080a230765a5dfbcc187aac4d0201f93ce0f3fc1
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-04-22 18:30:43 +0800

    Update K8s CSI post, small updates to IBM Snapshot post

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2123,7 +2123,7 @@ var store = [{
         "url": "/2026/01/19/netapp-eseries-santricity-csi.html",
         "teaser": null
       },{
-        "title": "Kubernetes with NetApp E-Series SANtricity",
+        "title": "Kubernetes CSI with NetApp E-Series SANtricity",
         "excerpt":"Introduction I wrote a post about CSI options with E-Series SANtricity systems more than three years ago. Almost everything from that post is true except SANtricity CSI is now available for testing and experimental use. This is an update that’s going to be short, and hopefully easier to figure out...","categories": ["netapp","storage","kubernetes"],
         "tags": ["csi","e-series","kubernetes","netapp","santricity"],
         "url": "/2026/01/20/kubernetes-netapp-eseries-santricity-csi.html",
@@ -2352,7 +2352,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Single volume snapshots on IBM Block CSI with SANtricity patch 2026-04-22 00:00 5 minute read How to create single volume snapshots in IBM Block CSI with SANtricity patch Splunk Operator for Kubernetes with NetApp E-Series 2026-04-21 00:00 6 minute read Splunk 10.2 installed by SOK on k0s with IBM Block...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Single volume snapshots on IBM Block CSI with SANtricity patch 2026-04-22 00:00 6 minute read How to create single volume snapshots in IBM Block CSI with SANtricity patch Splunk Operator for Kubernetes with NetApp E-Series 2026-04-21 00:00 6 minute read Splunk 10.2 installed by SOK on k0s with IBM Block...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
