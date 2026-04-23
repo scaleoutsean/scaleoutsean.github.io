850afc75a79376dc6dc30df20c6c09f86e882eda
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-04-23 15:33:18 +0800

    EPA 4 beta

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2339,6 +2339,12 @@ var store = [{
         "url": "/2026/04/22/single-volume-snapshot-ibm-block-csi-santricity.html",
         "teaser": null
       },{
+        "title": "EPA 4 development update",
+        "excerpt":"Introduction E-Series Performance Analyzer (EPA) was originally maintained by NetApp, which stopped maintaining the project more than four years ago. There was no signficant community activity around it, so I forked the project and have been maintaining it by myself since 2023. Although EPA has always been performance-focused, it had...","categories": ["storage","e-series","monitoring"],
+        "tags": ["netapp","epa","projects","santricity"],
+        "url": "/2026/04/23/epa_400_beta.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2352,7 +2358,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Single volume snapshots on IBM Block CSI with SANtricity patch 2026-04-22 00:00 6 minute read How to create single volume snapshots in IBM Block CSI with SANtricity patch Splunk Operator for Kubernetes with NetApp E-Series 2026-04-21 00:00 6 minute read Splunk 10.2 installed by SOK on k0s with IBM Block...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"EPA 4 development update 2026-04-23 00:00 9 minute read EPA 4 is close to public beta. This posts explains what will change in EPA 4 compared to EPA 3 Single volume snapshots on IBM Block CSI with SANtricity patch 2026-04-22 00:00 6 minute read How to create single volume snapshots...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Projects The following are my OSS repos except for E-Series Sizing Tools which is a (public) Web site (it’s a bit sensitive from a competitive perspective, so I haven’t posted that source to Github). The rest is all permissively licensed OSS. Repo Description All-in-One BeeGFS stack All-in-One BeeGFS stack with...","url": "https://scaleoutsean.github.io/projects.html"
@@ -4135,4 +4141,7 @@ var store = [{
   },{
     "title": "Home",
     "excerpt":"","url": "https://scaleoutsean.github.io/page65/"
+  },{
+    "title": "Home",
+    "excerpt":"","url": "https://scaleoutsean.github.io/page66/"
   }]
