8578e4ad4744f811bf471ff5a7f46c73622b6148
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-04-28 02:29:18 +0800

    Ubuntu 26.04 - add VDO content

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2370,7 +2370,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Ubuntu 26.04 LTS NVMe/RoCE with NetApp E-Series SANtricity 2026-04-28 00:00 11 minute read Ubuntu 26.04 LTS NVMe/RoCE without DOCA with NetApp E-Series (plus DM-MP and VDO) Forward E-Series SANtricity logs to Victoria Logs syslog receiver 2026-04-26 00:00 2 minute read How to forward SANtricity logs to Victoria Logs syslog receiver...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Ubuntu 26.04 LTS NVMe/RoCE with NetApp E-Series SANtricity 2026-04-28 00:00 12 minute read Ubuntu 26.04 LTS NVMe/RoCE without DOCA with NetApp E-Series (plus DM-MP and VDO) Forward E-Series SANtricity logs to Victoria Logs syslog receiver 2026-04-26 00:00 2 minute read How to forward SANtricity logs to Victoria Logs syslog receiver...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental thing (April 2026). I’m trying to (manually) create accessible presentation-like summaries in HTML format for situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Notes on E-Series with Proxmox (PVE, PBS) Projects Most of my work...","url": "https://scaleoutsean.github.io/projects.html"
