34a4a030357cb9e175061af97a588d5077863506
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-04-30 16:55:11 +0800

    Velero CSI snapshots with IBM Block CSI - add capacity section

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2376,7 +2376,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Velero CSI backup with SANtricity Snapshots and read-only Linked Clones 2026-04-30 00:00 15 minute read Not just Poor Man’s Backup - improved Velero functionality for SANtricity with IBM Block CSI with SANtricity patches 1.13.2 Ubuntu 26.04 LTS NVMe/RoCE with NetApp E-Series SANtricity 2026-04-28 00:00 12 minute read Ubuntu 26.04 LTS...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Velero CSI backup with SANtricity Snapshots and read-only Linked Clones 2026-04-30 00:00 17 minute read Not just Poor Man’s Backup - improved Velero functionality for SANtricity with IBM Block CSI with SANtricity patches 1.13.2 Ubuntu 26.04 LTS NVMe/RoCE with NetApp E-Series SANtricity 2026-04-28 00:00 12 minute read Ubuntu 26.04 LTS...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental thing (April 2026). I’m trying to (manually) create accessible presentation-like summaries in HTML format for situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Notes on E-Series with Proxmox (PVE, PBS) Projects Most of my work...","url": "https://scaleoutsean.github.io/projects.html"
