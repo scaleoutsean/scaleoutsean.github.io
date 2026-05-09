a3f491fe4bcc8df4f11f6bacf81217a23f2a6801
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-05-10 04:24:18 +0800

    PBS 4.2 add screenshot

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2394,7 +2394,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Proxmox Backup Server 4.2, Versity S3 Gateway and NetApp E-Series 2026-05-10 00:00 5 minute read Back both PBS 4.2+ block and S3 datastores with NetApp E-Series Cloud-Native Postgres with NetApp SANtricity and SolidFire CSI 2026-05-01 00:00 18 minute read Cloud-Native PostgreSQL with NetApp SANtricity and SolidFire CSI (VolumeSnapshots) Velero CSI...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Proxmox Backup Server 4.2, Versity S3 Gateway and NetApp E-Series 2026-05-10 00:00 6 minute read Back both PBS 4.2+ block and S3 datastores with NetApp E-Series Cloud-Native Postgres with NetApp SANtricity and SolidFire CSI 2026-05-01 00:00 18 minute read Cloud-Native PostgreSQL with NetApp SANtricity and SolidFire CSI (VolumeSnapshots) Velero CSI...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
