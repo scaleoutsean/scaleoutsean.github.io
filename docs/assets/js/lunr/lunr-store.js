ab287bc794fcc0db1b99dab9b87398ceb9d5d1f3
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-05-14 19:17:42 +0800

    Update Kasten post

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2412,7 +2412,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Kasten 8, SANtricity CSI 1 and NetApp E-Series 2026-05-12 00:00 3 minute read Backup SANtricity CSI with enterprise-grade backup - Kasten by Veeam Ubuntu MicroK8s with SANtricity CSI and NetApp E-Series 2026-05-12 00:00 9 minute read Overcome MicroK8s annoyances and set up SANtricity CSI driver for NetApp E-Series Storage monitoring...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Kasten 8, SANtricity CSI 1 and NetApp E-Series 2026-05-12 00:00 8 minute read Backup SANtricity CSI with enterprise-grade backup - Kasten by Veeam Ubuntu MicroK8s with SANtricity CSI and NetApp E-Series 2026-05-12 00:00 9 minute read Overcome MicroK8s annoyances and set up SANtricity CSI driver for NetApp E-Series Storage monitoring...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
