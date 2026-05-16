bc7021c7d6e50b867e65f17f0a8f3a82b062c797
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-05-16 13:23:46 +0800

    santricity-client Volume Copy - fix subtitle

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2418,7 +2418,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Volume Copy in santricity-client (Python) 2026-05-16 00:00 7 minute read santricity-client adds Kasten 8, SANtricity CSI 1 and NetApp E-Series 2026-05-12 00:00 14 minute read Backup SANtricity CSI with enterprise-grade backup - Kasten by Veeam Ubuntu MicroK8s with SANtricity CSI and NetApp E-Series 2026-05-12 00:00 9 minute read Overcome MicroK8s...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Volume Copy in santricity-client (Python) 2026-05-16 00:00 7 minute read santricity-client adds Volume Copy Kasten 8, SANtricity CSI 1 and NetApp E-Series 2026-05-12 00:00 14 minute read Backup SANtricity CSI with enterprise-grade backup - Kasten by Veeam Ubuntu MicroK8s with SANtricity CSI and NetApp E-Series 2026-05-12 00:00 9 minute read...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
