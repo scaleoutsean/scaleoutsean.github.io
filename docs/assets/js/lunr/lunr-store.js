4f54b73b085908b0e624e636470d02697359d79b
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-05-28 00:42:35 +0800

    Multi-node Qdrant - add SANtricity notes

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2448,7 +2448,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Multi-node Qdrant on Kubernetes with NetApp E-Series 2026-05-26 00:00 16 minute read Thoughts on Qdrant backup and restore and storage management on Kubernetes Rook Ceph on NetApp E-Series 2026-05-22 00:00 9 minute read Deploy Rook Ceph on CSI backed by NetApp E-Series SANmox updates for Proxmox-NetApp E-Series 2026-05-21 00:00 6...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Multi-node Qdrant on Kubernetes with NetApp E-Series 2026-05-26 00:00 18 minute read Thoughts on Qdrant backup and restore and storage management on Kubernetes Rook Ceph on NetApp E-Series 2026-05-22 00:00 9 minute read Deploy Rook Ceph on CSI backed by NetApp E-Series SANmox updates for Proxmox-NetApp E-Series 2026-05-21 00:00 6...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
