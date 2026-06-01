68da6368d55f7e9f51ccdfdb1ef2fa2b74bfcf8c
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-06-01 15:30:11 +0800

    CNPG performance

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2358,7 +2358,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Single node Qdrant vector database, Kubernetes and NetApp E-Series CSI",
-        "excerpt":"Part 1: Single-Node Qdrant on Kubernetes with NetApp E-Series (this post) Part 2: Multi-Node Qdrant on Kubernetes with NetApp E-Series (including Backup/Restore) Introduction It appears my IBM Block CSI patched for SANtricity can now do both FSB and CSI backups. Still, I did a very simple test for that post...","categories": ["storage","e-series","ai","kubernetes"],
+        "excerpt":"Part 1: Single-Node Qdrant on Kubernetes with NetApp E-Series (this post) Part 2: Multi-Node Qdrant on Kubernetes with NetApp E-Series (including Backup/Restore to/from S3) Introduction It appears my IBM Block CSI patched for SANtricity can now do both FSB and CSI backups. Still, I did a very simple test for...","categories": ["storage","e-series","ai","kubernetes"],
         "tags": ["vector-database","embeddings","velero","netapp","csi","qdrant"],
         "url": "/2026/04/30/singleton-qdrant-vector-db-netapp-eseries-santricity-csi.html",
         "teaser": null
@@ -2370,7 +2370,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Cloud-Native Postgres with NetApp SANtricity and SolidFire CSI",
-        "excerpt":"PART ONE: CloudNativePG with CSI snapshots (this post) PART TWO: CloudNativePG with plugins (future post) Introduction The number of things known-to-work with IBM Block CSI patched for SANtricity is increasing: so far “first pass” CSI tests have been completed for Versity S3 Gateway, Splunk, Elasticsearch, and Qdrant. Thanks to a...","categories": ["storage","e-series","solidfire","kubernetes"],
+        "excerpt":"PART ONE: CloudNativePG with CSI snapshots (this post) PART TWO: CloudNativePG with plugins (future post) PART THREE: CloudNativePG performance test with IBM Block CSI and NVMe/ROCE Introduction The number of things known-to-work with IBM Block CSI patched for SANtricity is increasing: so far “first pass” CSI tests have been completed...","categories": ["storage","e-series","solidfire","kubernetes"],
         "tags": ["cloudnativepg","postgresql","database","netapp","ibm","csi"],
         "url": "/2026/05/01/cloud-native-postgres-kuberntes-netapp-eseries-csi.html",
         "teaser": null
@@ -2447,6 +2447,12 @@ var store = [{
         "url": "/2026/05/29/microsoft-sql-server-kubernetes-netapp-eseries.html",
         "teaser": null
       },{
+        "title": "Cloud-Native Postgres Performance with NetApp SANtricity NVMe/RoCE",
+        "excerpt":"PART ONE: CloudNativePG with CSI snapshots PART TWO: CloudNativePG with plugins (future post) PART THREE: CloudNativePG performance test with IBM Block CSI and NVMe/ROCE (this post) Introduction In the first post, I didn’t have time to evaluate performance. I still don’t, but at least I’ve done a worst-case scenario test...","categories": ["storage","e-series","kubernetes"],
+        "tags": ["cloudnativepg","postgresql","database","netapp","performance","ibm","csi"],
+        "url": "/2026/06/01/cloud-native-postgres-kuberntes-netapp-eseries-perf.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2460,7 +2466,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"MS SQL Server on Kubernetes with NetApp E-Series 2026-05-29 00:00 7 minute read How to deploy and connect to SQL Server Stateful Set on Kubernetes with E-Series Firemox (Proxmox-SolidFire TUI) updates 2026-05-27 00:00 1 minute read What’s new in Firemox in May 2026 Multi-node Qdrant on Kubernetes with NetApp E-Series...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Cloud-Native Postgres Performance with NetApp SANtricity NVMe/RoCE 2026-06-01 00:00 4 minute read PART ONE: CloudNativePG with CSI snapshots PART TWO: CloudNativePG with plugins (future post) PART THREE: CloudNativePG performance test with IBM Bloc... MS SQL Server on Kubernetes with NetApp E-Series 2026-05-29 00:00 7 minute read How to deploy and...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
@@ -4291,4 +4297,7 @@ var store = [{
   },{
     "title": "Home",
     "excerpt":"","url": "https://scaleoutsean.github.io/page68/"
+  },{
+    "title": "Home",
+    "excerpt":"","url": "https://scaleoutsean.github.io/page69/"
   }]
