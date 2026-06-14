8dd406b70ac044d791694b613dc03e57b105fd48
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-06-14 23:11:46 +0800

    sg-cosi RO snapshots - fix typos, add Rumble demo

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2502,7 +2502,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Does OpenSharing have anything for NetApp E-Series? 2026-06-14 00:00 8 minute read Does OpenSharing matter to E-Series users? StorageGRID bucket snapshots with sg-cosi 2026-06-13 00:00 4 minute read How to automate creation of read-only snapshot buckets and credential rotation with sg-cosi COSI and sg-cosi 2026-06-07 00:00 18 minute read Why...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Does OpenSharing have anything for NetApp E-Series? 2026-06-14 00:00 9 minute read Does OpenSharing matter to E-Series users? StorageGRID bucket snapshots with sg-cosi 2026-06-13 00:00 5 minute read How to automate creation of read-only snapshot buckets and credential rotation with sg-cosi COSI and sg-cosi 2026-06-07 00:00 18 minute read Why...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
