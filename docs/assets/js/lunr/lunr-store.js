6dd84acfa991dac8b26d07e3af463e9298cca9bf
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-06-17 13:37:17 +0800

    OpenSharing Volumes

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2501,8 +2501,8 @@ var store = [{
         "url": "/2026/06/16/netapp-eseries-iot-compaction-opensharing.html",
         "teaser": null
       },{
-        "title": "Share NetApp E-Series files with OpenSharing and Versity S3 Gateway",
-        "excerpt":"Introduction The other day I blogged about OpenSharing and E-Series and mentioned how Tables are where all of the action must be right now, but Volumes are more (positively) disruptive for E-Series because if you have data on an E-Series volume and want to share it, you’re not in a...","categories": ["netapp","storage","analytics"],
+        "title": "NetApp volume content sharing with OpenSharing and Versity S3 Gateway",
+        "excerpt":"Introduction The other day I blogged about OpenSharing and E-Series and mentioned how Tables are where almost all of the action must be right now, but Volumes are more (positively) disruptive for E-Series because today, if you have data on an E-Series volume and want to share it, you’re not...","categories": ["netapp","storage","analytics"],
         "tags": ["analytics","e-series","opensharing","volumes","versity","file-sharing"],
         "url": "/2026/06/17/opensharing-files-versity-s3-gateway-netapp-eseries.html",
         "teaser": null
@@ -2520,7 +2520,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Share NetApp E-Series files with OpenSharing and Versity S3 Gateway 2026-06-17 00:00 4 minute read Introduction IoT ingress, OpenSharing, Versity S3 Gateway and NetApp E-Series 2026-06-16 00:00 8 minute read We take a look at packaging JSON data for OpenSharing-ready serving with Versity S3 Gateway and NetApp E-Series Iceberg table...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"NetApp volume content sharing with OpenSharing and Versity S3 Gateway 2026-06-17 00:00 4 minute read After Tables, we evaluate OpenSharing API for NetApp E-Series Volume assets IoT ingress, OpenSharing, Versity S3 Gateway and NetApp E-Series 2026-06-16 00:00 8 minute read We take a look at packaging JSON data for OpenSharing-ready...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
