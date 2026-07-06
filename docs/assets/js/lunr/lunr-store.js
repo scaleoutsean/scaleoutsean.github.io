eace0ddea4f10540d8a845f63d0a928682244212
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-07-06 12:24:12 +0800

    EKE v0.1 - improve text

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2556,7 +2556,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Eke - CLI utility for smarter data management with BeeGFS on NetApp E-Series 2026-07-05 00:00 12 minute read Eke is my take on a subset BeeGFS CLI features and some extra goodies Kompromise - control plane for S3 data pipelines with NetApp StorageGRID/E-Series and Versity S3 Gateway 2026-06-27 00:00 10...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Eke - CLI utility for smarter data management with BeeGFS on NetApp E-Series 2026-07-05 00:00 13 minute read Eke is my take on a subset BeeGFS CLI features and some extra goodies Kompromise - control plane for S3 data pipelines with NetApp StorageGRID/E-Series and Versity S3 Gateway 2026-06-27 00:00 10...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
