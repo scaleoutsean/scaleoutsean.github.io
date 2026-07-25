2031a804220cd4a3dbcba0970294f11109770b62
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-07-25 19:08:53 +0800

    SG 12.1 and cosi 0.5.4 - comment on future dates

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2598,7 +2598,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Tier WEKA NeuralMesh data to NetApp StorageGRID S3 Object Store 2026-07-25 00:00 7 minute read Get the benefits of both WEKA and StorageGRID by using storage tiering on WEKA What’s new in NetApp StorageGRID 12.1 and sg-cosi 0.5.4 2026-07-25 00:00 3 minute read StorageGRID 12.1 OpenSharing server for NetApp StorageGRID...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Tier WEKA NeuralMesh data to NetApp StorageGRID S3 Object Store 2026-07-25 00:00 7 minute read Get the benefits of both WEKA and StorageGRID by using storage tiering on WEKA What’s new in NetApp StorageGRID 12.1 and sg-cosi 0.5.4 2026-07-25 00:00 4 minute read StorageGRID 12.1 OpenSharing server for NetApp StorageGRID...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
