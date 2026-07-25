127a0b9e0e9a5e2199dd6d173cfb7ac26d791172
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-07-25 11:35:42 +0800

    WEKA with SG - add blog post description

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2592,7 +2592,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Tier WEKA NeuralMesh data to NetApp StorageGRID S3 Object Store 2026-07-25 00:00 4 minute read Introduction OpenSharing server for NetApp StorageGRID and E-Series with Versity S3 Gateway 2026-07-22 00:00 4 minute read OpenSharing server for StorageGRID and E-Series with VGW done in Go What’s new in BeeGFS 8.4 2026-07-18 00:00...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Tier WEKA NeuralMesh data to NetApp StorageGRID S3 Object Store 2026-07-25 00:00 4 minute read Get the benefits of both WEKA and StorageGRID by using storage tiering on WEKA OpenSharing server for NetApp StorageGRID and E-Series with Versity S3 Gateway 2026-07-22 00:00 4 minute read OpenSharing server for StorageGRID and...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
