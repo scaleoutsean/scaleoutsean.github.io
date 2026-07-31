e49b958a42ff0c4d498bc6676bb25e6f4b71c21d
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-07-31 22:55:18 +0800

    Snapshotless backup - ES 8 demo in appendix

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2610,7 +2610,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Backup and restore modern Kubernetes applications without CSI snapshots 2026-07-30 00:00 6 minute read IBM Block CSI with SANtricity patch has CSI snapshots but I’ve been advocating against CSI snapshots because you probably can do better with backup to S3 What’s new in Kasten K10 9.0 2026-07-29 00:00 3 minute...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Backup and restore modern Kubernetes applications without CSI snapshots 2026-07-30 00:00 12 minute read IBM Block CSI with SANtricity patch has CSI snapshots but I’ve been advocating against CSI snapshots because you probably can do better with backup to S3 What’s new in Kasten K10 9.0 2026-07-29 00:00 3 minute...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
