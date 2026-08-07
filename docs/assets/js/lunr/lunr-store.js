6dd3046558691afbfa52045f20cbbc7dc29d9920
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-08-08 00:15:59 +0800

    VGW S3/RDMA

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2633,6 +2633,12 @@ var store = [{
         "url": "/2026/08/07/simple-access-to-beegfs-rst-data.html",
         "teaser": null
       },{
+        "title": "S3/RDMA with Versity S3 Gateway and NetApp E-Series",
+        "excerpt":"Introduction I’ve written a number of posts about the Versity S3 Gateway with NetApp E-Series. I think it’s a great combination. Earlier this week Versity committed S3/RDMA code to the Versity S3 Gateway repository. Is that useful? Can we use it with E-Series? How? Let’s take a crack at these...","categories": ["storage","eseries","netapp","s3"],
+        "tags": ["versity","s3","rdma","vgw"],
+        "url": "/2026/08/08/versity-s3-rdma-with-netapp-eseries.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2646,7 +2652,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Directly access BeeGFS RST data on NetApp StorageGRID 2026-08-07 00:00 5 minute read How to enable secure access to BeeGFS RST-tiered data on StorageGRID from anywhere you need Achieving Full Stripe Width Acceleration (FSWA) on SANtricity 2026-08-07 00:00 7 minute read How to write to a LUN and make sure...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"S3/RDMA with Versity S3 Gateway and NetApp E-Series 2026-08-08 00:00 2 minute read Getting S3/RDMA with Versity S3 Gateway and NetApp SANtricity Directly access BeeGFS RST data on NetApp StorageGRID 2026-08-07 00:00 5 minute read How to enable secure access to BeeGFS RST-tiered data on StorageGRID from anywhere you need...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
@@ -4306,6 +4312,9 @@ var store = [{
   },{
     "title": "hybrid-cloud",
     "excerpt":"","url": "https://scaleoutsean.github.io/tags/hybrid-cloud/"
+  },{
+    "title": "vgw",
+    "excerpt":"","url": "https://scaleoutsean.github.io/tags/vgw/"
   },{
     "title": "projects",
     "excerpt":"","url": "https://scaleoutsean.github.io/categories/projects/"
