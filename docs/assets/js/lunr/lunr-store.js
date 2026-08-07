38341844888574173a8f27dde24db564ef9d93d8
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-08-07 22:31:47 +0800

    RST with SG - clarify OneLake workflow

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2646,7 +2646,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Directly access BeeGFS RST data on NetApp StorageGRID 2026-08-07 00:00 4 minute read How to enable secure access to BeeGFS RST-tiered data on StorageGRID from anywhere you need Achieving Full Stripe Width Acceleration (FSWA) on SANtricity 2026-08-07 00:00 7 minute read How to write to a LUN and make sure...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Directly access BeeGFS RST data on NetApp StorageGRID 2026-08-07 00:00 5 minute read How to enable secure access to BeeGFS RST-tiered data on StorageGRID from anywhere you need Achieving Full Stripe Width Acceleration (FSWA) on SANtricity 2026-08-07 00:00 7 minute read How to write to a LUN and make sure...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
