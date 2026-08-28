ca741df2bde00d6d684c907ebda14d7f6fc7cc99
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-08-28 17:24:38 +0800

    Agentic RAG - add fixed tool info

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2700,7 +2700,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Local agentic RAG with (only) NetApp StorageGRID S3 2026-08-28 00:00 13 minute read The post discusses the trend of hosting AI-related persistent data for agentic RAG on S3 object stores Starfish Storage for better StorageGRID and BeeGFS manageability 2026-08-27 00:00 5 minute read This post is a simple overview of...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Local agentic RAG with (only) NetApp StorageGRID S3 2026-08-28 00:00 14 minute read The post discusses the trend of hosting AI-related persistent data for agentic RAG on S3 object stores Starfish Storage for better StorageGRID and BeeGFS manageability 2026-08-27 00:00 5 minute read This post is a simple overview of...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
