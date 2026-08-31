9a3730dccf92e3e6669f5d967ef433516ed9ce35
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-08-31 15:20:46 +0800

    DuckLabs

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2687,6 +2687,12 @@ var store = [{
         "url": "/2026/08/28/datalake-agentic-rag-netapp-storagegrid.html",
         "teaser": null
       },{
+        "title": "DuckDB and AWS",
+        "excerpt":"DuckLabs are getting acquired by AWS. The press release says: Together, we’ll make AWS analytics faster, simpler, and more cost-effective for customers. I’ve been using DuckDB for a while, and even in the recent blog posts. DuckDB works well with S3, it’s very fast and provides a superior developer and...","categories": ["storage","netapp","storagegrid","s3","ai","analytics"],
+        "tags": ["duckdb","spark"],
+        "url": "/2026/08/31/duckdb-tpch.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2700,7 +2706,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Local agentic RAG with (only) NetApp StorageGRID S3 2026-08-28 00:00 14 minute read The post discusses the trend of hosting AI-related persistent data for agentic RAG on S3 object stores Starfish Storage for better StorageGRID and BeeGFS manageability 2026-08-27 00:00 5 minute read This post is a simple overview of...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"DuckDB and AWS 2026-08-31 00:00 3 minute read Good move for AWS and DuckLabs, bad news for Spark Local agentic RAG with (only) NetApp StorageGRID S3 2026-08-28 00:00 14 minute read The post discusses the trend of hosting AI-related persistent data for agentic RAG on S3 object stores Starfish Storage...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
@@ -4397,6 +4403,9 @@ var store = [{
     "title": "hpc",
     "excerpt":"","url": "https://scaleoutsean.github.io/tags/hpc/"
   },{
+    "title": "duckdb",
+    "excerpt":"","url": "https://scaleoutsean.github.io/tags/duckdb/"
+  },{
     "title": "projects",
     "excerpt":"","url": "https://scaleoutsean.github.io/categories/projects/"
   },{
@@ -4666,4 +4675,7 @@ var store = [{
   },{
     "title": "Home",
     "excerpt":"","url": "https://scaleoutsean.github.io/page64/"
+  },{
+    "title": "Home",
+    "excerpt":"","url": "https://scaleoutsean.github.io/page65/"
   }]
