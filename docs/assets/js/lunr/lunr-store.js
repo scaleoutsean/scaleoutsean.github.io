dd4a5389a32dc9f9a872095b052d594fc5f1cf79
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-09-01 11:31:49 +0800

    DuckDB - add temp/cache and fio control runs

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2706,7 +2706,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"AWS buys DuckLabs and about DuckDB with NetApp storage 2026-08-31 00:00 8 minute read Good move for AWS, DuckLabs and users, bad news for Spark. Also, why StorageGRID and E-Series for DuckDB Local agentic RAG with (only) NetApp StorageGRID S3 2026-08-28 00:00 14 minute read The post discusses the trend...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"AWS buys DuckLabs and about DuckDB with NetApp storage 2026-08-31 00:00 11 minute read Good move for AWS, DuckLabs and users, bad news for Spark. Also, why StorageGRID and E-Series for DuckDB Local agentic RAG with (only) NetApp StorageGRID S3 2026-08-28 00:00 14 minute read The post discusses the trend...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
