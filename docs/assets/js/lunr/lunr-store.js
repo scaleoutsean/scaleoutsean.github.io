1d45080be9ef45fa057c04406451919ba5a49aa4
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-09-04 01:53:54 +0800

    Alluxio 2026 - improve text

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2718,7 +2718,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"AI and analytics in hybrid cloud with Alluxio and NetApp StorageGRID 2026-09-04 00:00 6 minute read Solve performance and data management in hybrid cloud use cases Lakekeeper with NetApp StorageGRID 2026-09-03 00:00 4 minute read Why Lakekeeper with NetApp StorageGRID AWS buys DuckLabs and about DuckDB with NetApp storage 2026-08-31...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"AI and analytics in hybrid cloud with Alluxio and NetApp StorageGRID 2026-09-04 00:00 7 minute read Solve performance and data management in hybrid cloud use cases Lakekeeper with NetApp StorageGRID 2026-09-03 00:00 4 minute read Why Lakekeeper with NetApp StorageGRID AWS buys DuckLabs and about DuckDB with NetApp storage 2026-08-31...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
