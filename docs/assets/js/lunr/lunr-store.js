92b24111a8ec8fc2a099f478e8257817dc38ad1b
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-09-14 02:28:15 +0800

    DuckDB Quack with E/SG

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2717,6 +2717,12 @@ var store = [{
         "url": "/2026/09/08/opensharing-data-mobility-netapp-eseries-storagegrid.html",
         "teaser": null
       },{
+        "title": "DuckDB Quack with NetApp E-Series",
+        "excerpt":"Introduction In a recent post I wrote about DuckDB with E-Series (and ran some casual performance tests). What I didn’t do is focus more on the new Quack server, how it differs from hosting regular “in-process” DuckDB on block storage, and a NetApp E-Series options You have TBs of DuckDB...","categories": ["storage","netapp","storagegrid","s3","ai","analytics","e-series"],
+        "tags": ["duckdb","quack","atalake","iceberg","parquet"],
+        "url": "/2026/09/14/duckdb-quack-netapp-storage.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2730,7 +2736,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"The elusive data mobility for tables with OpenSharing, StorageGRID, E-Series 2026-09-08 00:00 5 minute read As we all know, data sharing in hybrid cloud is a solved problem thanks to S3. Here’s yet another example. OpenSharing Iceberg tables on NetApp StorageGRID and E-Series 2026-09-07 00:00 4 minute read Run OpenSharing...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"DuckDB Quack with NetApp E-Series 2026-09-14 00:00 4 minute read Introduction The elusive data mobility for tables with OpenSharing, StorageGRID, E-Series 2026-09-08 00:00 5 minute read As we all know, data sharing in hybrid cloud is a solved problem thanks to S3. Here’s yet another example. OpenSharing Iceberg tables on...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
@@ -4432,6 +4438,12 @@ var store = [{
   },{
     "title": "trino",
     "excerpt":"","url": "https://scaleoutsean.github.io/tags/trino/"
+  },{
+    "title": "quack",
+    "excerpt":"","url": "https://scaleoutsean.github.io/tags/quack/"
+  },{
+    "title": "atalake",
+    "excerpt":"","url": "https://scaleoutsean.github.io/tags/atalake/"
   },{
     "title": "projects",
     "excerpt":"","url": "https://scaleoutsean.github.io/categories/projects/"
