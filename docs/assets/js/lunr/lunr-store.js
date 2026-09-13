343ffe59645246cd020f3fd40bd46e5ff0840705
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-09-14 02:40:26 +0800

    DuckDB Quack with E/SG - fix typos add perf section

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2736,7 +2736,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"DuckDB Quack with NetApp E-Series 2026-09-14 00:00 4 minute read Introduction The elusive data mobility for tables with OpenSharing, StorageGRID, E-Series 2026-09-08 00:00 5 minute read As we all know, data sharing in hybrid cloud is a solved problem thanks to S3. Here’s yet another example. OpenSharing Iceberg tables on...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"DuckDB Quack with NetApp E-Series 2026-09-14 00:00 5 minute read What changes and what stays the same, plus setup and anecdotal performance tests The elusive data mobility for tables with OpenSharing, StorageGRID, E-Series 2026-09-08 00:00 5 minute read As we all know, data sharing in hybrid cloud is a solved...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
