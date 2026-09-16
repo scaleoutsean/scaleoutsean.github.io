d78ac2b982cb59206c97029704294077461dbb21
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-09-16 14:43:06 +0800

    Ray framework

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2729,6 +2729,12 @@ var store = [{
         "url": "/2026/09/14/sgac-storagegrid-audit-v024.html",
         "teaser": null
       },{
+        "title": "Ray framework with NetApp E-Series and StorageGRID",
+        "excerpt":"What is Ray They say… Ray is an open source unified framework for scaling AI and Python applications. It provides a simple, universal API for building distributed applications that can scale from a laptop to a cluster. Why should NetApp users care Well, they don’t have to care, but if...","categories": ["projects","storage","netapp","storagegrid","s3"],
+        "tags": ["audit","log","sgac","compliance","access"],
+        "url": "/2026/09/16/ray-framework-netapp-storagegrid-eseries.html",
+        "teaser": null
+      },{
     "title": "About this site",
     "excerpt":"About me I create solutions. The blog title and motto come from the theory of human action (praxeology). Disclaimer The content of this site and opinions expressed on it are personal. The site does not provide technical or other advice to my employer’s customers or other people out there. Content...","url": "https://scaleoutsean.github.io/about/"
   },{
@@ -2742,7 +2748,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"SGAC v0.2.4 2026-09-14 00:00 1 minute read What was changed and why DuckDB Quack server with NetApp E-Series 2026-09-14 00:00 9 minute read What changes and what stays the same, plus setup and anecdotal performance tests The elusive data mobility for tables with OpenSharing, StorageGRID, E-Series 2026-09-08 00:00 5 minute...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Ray framework with NetApp E-Series and StorageGRID 2026-09-16 00:00 6 minute read Use cases for Ray with StorageGRID and E-Series SGAC v0.2.4 2026-09-14 00:00 1 minute read What was changed and why DuckDB Quack server with NetApp E-Series 2026-09-14 00:00 9 minute read What changes and what stays the same,...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
@@ -4723,4 +4729,7 @@ var store = [{
   },{
     "title": "Home",
     "excerpt":"","url": "https://scaleoutsean.github.io/page65/"
+  },{
+    "title": "Home",
+    "excerpt":"","url": "https://scaleoutsean.github.io/page66/"
   }]
