ce6f8db28bf9c153b0612fb45852c680b43edd58
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-09-18 14:50:27 +0800

    Ray - add BeeGFS checkpointing

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2730,8 +2730,8 @@ var store = [{
         "teaser": null
       },{
         "title": "Ray framework with NetApp E-Series and StorageGRID",
-        "excerpt":"What is Ray They say… Ray is an open source unified framework for scaling AI and Python applications. It provides a simple, universal API for building distributed applications that can scale from a laptop to a cluster. Why should NetApp users care Well, they don’t have to care, but if...","categories": ["projects","storage","netapp","storagegrid","s3"],
-        "tags": ["audit","log","sgac","compliance","access"],
+        "excerpt":"What is Ray They say… Ray is an open source unified framework for scaling AI and Python applications. It provides a simple, universal API for building distributed applications that can scale from a laptop to a cluster. Why should NetApp users care Well, they don’t have to care, but if...","categories": ["projects","storage","netapp","storagegrid","s3","ai"],
+        "tags": ["ray","ml","analytics","beegfs"],
         "url": "/2026/09/16/ray-framework-netapp-storagegrid-eseries.html",
         "teaser": null
       },{
@@ -2754,7 +2754,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"Enable PiT backup of NetApp StorageGRID buckets with sg-cosi 2026-09-18 00:00 6 minute read Use sg-cosi to implement poor man’s S3 bucket backups with the fancy Point-in-Time feature Ray framework with NetApp E-Series and StorageGRID 2026-09-16 00:00 9 minute read Use cases for Ray with StorageGRID and E-Series SGAC v0.2.4...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"Enable PiT backup of NetApp StorageGRID buckets with sg-cosi 2026-09-18 00:00 6 minute read Use sg-cosi to implement a poor man’s S3 bucket backups with the fancy Point-in-Time feature Ray framework with NetApp E-Series and StorageGRID 2026-09-16 00:00 13 minute read Use cases for Ray with StorageGRID and E-Series SGAC...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
@@ -4462,6 +4462,12 @@ var store = [{
   },{
     "title": "access",
     "excerpt":"","url": "https://scaleoutsean.github.io/tags/access/"
+  },{
+    "title": "ray",
+    "excerpt":"","url": "https://scaleoutsean.github.io/tags/ray/"
+  },{
+    "title": "ml",
+    "excerpt":"","url": "https://scaleoutsean.github.io/tags/ml/"
   },{
     "title": "projects",
     "excerpt":"","url": "https://scaleoutsean.github.io/categories/projects/"
