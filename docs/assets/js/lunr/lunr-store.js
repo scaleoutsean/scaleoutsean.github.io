2d1da08d234495f11a75a08ecf347b99fce5d377
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-09-25 02:43:05 +0800

    Improve NVMe ANA details in NVMe/RoCE post

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2778,7 +2778,7 @@ var store = [{
     "excerpt":"About this site Posts by Category Home Page Archive Post Archive Projects Posts by Tag Posts by Year hci kubernetes netapp rancher solidfire github dark mode certificate postman devops plugin provider terraform hcc log logging powershell qos e-series eseries fc fibre channel iscsi performance api python f5 ingress nginx backup...","url": "https://scaleoutsean.github.io/page-archive/"
   },{
     "title": "Post Archive",
-    "excerpt":"OpenSharing E-Series data using Assume Role With WebIdentity 2026-09-24 00:00 2 minute read Quick post explores another win for the thinking E-Series user YugabyteDB with NetApp EF-Series storage 2026-09-21 00:00 15 minute read We walk through TFM and look for tips and tricks relevant to EF-Series SGAC v0.3.0 2026-09-20 00:00...","url": "https://scaleoutsean.github.io/archive/"
+    "excerpt":"OpenSharing E-Series data using Assume Role With WebIdentity 2026-09-24 00:00 3 minute read Quick post explores another win for the thinking E-Series user YugabyteDB with NetApp EF-Series storage 2026-09-21 00:00 15 minute read We walk through TFM and look for tips and tricks relevant to EF-Series SGAC v0.3.0 2026-09-20 00:00...","url": "https://scaleoutsean.github.io/archive/"
   },{
     "title": "Projects",
     "excerpt":"Presentations This is a recent and experimental section added in April 2026. I manually create simple, accessible presentation-like notes in the HTML format for the situations where sharing in the browser is better overall. Presentation Description NetApp E-Series in Proxmox Ecosystem Presentation on NetApp E-Series with Proxmox (PVE, PBS) NetApp...","url": "https://scaleoutsean.github.io/projects.html"
